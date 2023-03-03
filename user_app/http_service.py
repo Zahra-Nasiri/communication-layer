0ce1952ca139a9a3687eebfe163fe270efffbd17
Zahra-Nasiri <z.nasir.8181@gmail.com>
2023-03-03 16:30:42 +0330

    Implemented user login and user partial update API.

--- user_app/http_service.py
+++ user_app/http_service.py
@@ -14,3 +14,17 @@ class UserHttpService:
     async def register(self, user: User):
         request = requests.post(f"{self.user_service_url}/register", json=user.dict())
         return request.json()
+
+    async def login(self, user: LoginUser):
+        request = requests.post(f"{self.user_service_url}/login", json=user.dict())
+        return request.json()
+
+    async def partial_update_user(self, uid: str, user: UpdateUser):
+        request = requests.patch(f"{self.user_service_url}/{uid}", json=user.dict())
+        return request.json()
+
+    async def get_user_by_token(self, token: str):
+        request = requests.get(f"{self.user_service_url}/",
+                headers={'Authorization': f"Bearer {token}"},
+        )
+        return request.json()
--- user_app/router.py
+++ user_app/router.py
@@ -1,7 +1,8 @@
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from .models import User
+from .models import User, LoginUser, UpdateUser
 from .http_service import UserHttpService
+from fastapi import Header
 
 router = InferringRouter()
 http_service = UserHttpService()
@@ -12,3 +13,12 @@ class UserRouter:
     @router.post("/register")
     async def register(self, user: User):
         return await http_service.register(user)
+
+    @router.post("/login")
+    async def login(self, user: LoginUser):
+        return await http_service.login(user)
+
+    @router.patch("/")
+    async def partial_update_user(self, user: UpdateUser, token: str=Header()):
+        user_object = await http_service.get_user_by_token(token)
+        return await http_service.partial_update_user(user_object["_id"], user)
